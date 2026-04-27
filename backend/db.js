c8e712965c2f4bc7c5d962c77ed4c5ba773b742b
Carlos Velazquez <gabrielvelaz16@fpuna.edu.py>
2026-04-26 22:33:22 -0300

    Mejoras en dashboard y modal de voluntarios

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'VoluntApp_DB',   // ⚠️ cambia por tu BD real
-    password: '130499', // ⚠️ el que pusiste en pgAdmin
+    database: 'VoluntApp_DB', //llamada a la BASE DE DATOS
+    password: '130499', //el que pusiste en pgAdmin
     port: 5432,
 });
 
